55fa66c88480da0d956914259905964f86e326d4
hariom38 <rajpoothariom900@gmail.com>
2026-05-02 17:16:02 +0530

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -4,46 +4,66 @@
 // You must do this by modifying the input array in-place with O(1) extra memory.
 
 // Example 1:
-
 // Input: s = ["h","e","l","l","o"]
 // Output: ["o","l","l","e","h"]
-// Example 2:
 
+// Example 2:
 // Input: s = ["H","a","n","n","a","h"]
 // Output: ["h","a","n","n","a","H"]
-
-
-  
-Code
-Testcase
-Testcase
-Test Result
-344. Reverse String
-Solved
-Easy
-Topics
-premium lock icon
-Companies
-Hint
-Write a function that reverses a string. The input string is given as an array of characters s.
-
-You must do this by modifying the input array in-place with O(1) extra memory.
-
- 
-
-Example 1:
-
-Input: s = ["h","e","l","l","o"]
-Output: ["o","l","l","e","h"]
-Example 2:
-
-Input: s = ["H","a","n","n","a","h"]
-Output: ["h","a","n","n","a","H"]
- 
 
 Constraints:
 
 1 <= s.length <= 105
 s[i] is a printable ascii charact
 
+
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        //method 1-->
+        //in actually we are reversing  an array 
+        reverse(s.begin(),s.end());
+    }
+};
+// TC = O(n)
+// SC = O(1)
+
+
+
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        vector<char>temp;
+        //pushing string character in array 
+        for(int i=s.size()-1;i>=0;i--){
+            temp.push_back(s[i]);
+        }
+
+        s=temp;
+        
+    }
+};
+//tc----->o(n)
+//sc----->o(n)
+
+
+
+// Most optimized 
+
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+
+        int i=0;
+        int j=s.size()-1;
+        while(i<=j){
+            swap(s[i],s[j]);
+            i++;
+            j--;
+        }
+
+    }
+};
+//tc------>o(n)
+//sc------>o(1)
   
