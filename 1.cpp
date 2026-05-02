1cba5a23f557472e7856ba15eaa1e902a950e7b6
hariom38 <rajpoothariom900@gmail.com>
2026-05-02 17:14:36 +0530

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -1,7 +1,35 @@
 
+// Write a function that reverses a string. The input string is given as an array of characters s.
+
+// You must do this by modifying the input array in-place with O(1) extra memory.
+
+// Example 1:
+
+// Input: s = ["h","e","l","l","o"]
+// Output: ["o","l","l","e","h"]
+// Example 2:
+
+// Input: s = ["H","a","n","n","a","h"]
+// Output: ["h","a","n","n","a","H"]
+
+
+  
+Code
+Testcase
+Testcase
+Test Result
+344. Reverse String
+Solved
+Easy
+Topics
+premium lock icon
+Companies
+Hint
 Write a function that reverses a string. The input string is given as an array of characters s.
 
 You must do this by modifying the input array in-place with O(1) extra memory.
+
+ 
 
 Example 1:
 
@@ -11,6 +39,11 @@ Example 2:
 
 Input: s = ["H","a","n","n","a","h"]
 Output: ["h","a","n","n","a","H"]
+ 
 
+Constraints:
+
+1 <= s.length <= 105
+s[i] is a printable ascii charact
 
   
